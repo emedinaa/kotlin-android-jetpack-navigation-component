8ac3564eb0b2a9e6a8fed02ab25b30074961cdef
Eduardo José Medina Alfaro <emedinaa@gmail.com>
2020-08-17 18:41:26 -0500

    Added WorkingWithMenu project

---
+++ WorkingWithMenu/app/src/main/java/com/emedinaa/navigation/samples/Extensions.kt
@@ -0,0 +1,10 @@
+package com.emedinaa.navigation.samples
+
+import android.util.Log
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+fun Fragment.toast(message: String) {
+    Log.d("CONSOLE", message)
+    Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
+}
